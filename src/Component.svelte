70c6ef67e65d27941a0c1a90440ebb36d0329b5d
Liam Keane <liamkeane6@yahoo.co.uk>
2022-10-01 13:11:55 +0100

    First Change

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,12 +1,15 @@
 <script>
   import { getContext } from "svelte"
 
-  export let text
+  export let colourValue = 'ff00ff'
+
 
   const { styleable } = getContext("sdk")
   const component = getContext("component")
 </script>
 
 <div use:styleable={$component.styles}>
-  This is a custom component. Hello!.
+  {colourValue}
+  <div style="background-color: #{colourValue}; width:200px; height:200px;">
+  </div>
 </div>
